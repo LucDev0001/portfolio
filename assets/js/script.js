c09406ea4effaa732a4ecbf0028bf0ea200a9968
LucDev0001 <156922180+LucDev0001@users.noreply.github.com>
2024-02-01 17:30:48 -0300

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+
+function abre__pagina(){
+    document.querySelector(a);
+    abre__pagina.innerHTML = texto;
+    exibirTextoNaTela('h1', 'Sobre mim');
+    exibirTextoNaTela('p', 'Olá, sou o Luciano, um profissional determinado de 27 anos. Iniciei minha jornada aos 18 anos, saindo de Pernambuco em direção ao Rio de Janeiro. Ao longo dos últimos 5 anos, tenho dedicado meu tempo e esforço para alcançar meus objetivos. Atualmente, estou cursando Análise e Desenvolvimento de Sistemas na Universidade Veiga de Almeida, onde tenho a oportunidade de aprimorar minhas habilidades e conhecimentos na área de desenvolvimento. Com uma mentalidade focada no aprendizado contínuo, tenho buscado constantemente me especializar para estar sempre atualizado com as últimas tendências e tecnologias.  Meu compromisso com o crescimento profissional é evidenciado pelo meu envolvimento em projetos desafiadores e pelo constante investimento em meu desenvolvimento. Estou agora em busca de novas oportunidades de emprego na área de desenvolvimento, onde possa aplicar e expandir minhas habilidades, contribuindo para o sucesso da equipe e alcançando metas ambiciosas. Estou animado para fazer parte de uma equipe inovadora e dinâmica, onde posso continuar a crescer e prosperar');
+}
+
+
+    
+
